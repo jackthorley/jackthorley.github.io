3b958d2c303d693773e7a3922a9998e8f37647de
Jack Thorley <jackthorley89@gmail.com>
2023-10-18 22:35:46 +0100

    Add historical data

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-(function () {
+(async function () {
 
+  const plugin = {
+    id: 'customCanvasBackgroundColor',
+    beforeDraw: (chart, args, options) => {
+      const { ctx } = chart;
+      ctx.save();
+      ctx.globalCompositeOperation = 'destination-over';
+      ctx.fillStyle = options.color || '#99ffff';
+      ctx.fillRect(0, 0, chart.width, chart.height);
+      ctx.restore();
+    }
+  };
 
   async function getElec(region) {
     const response = await fetch(`https://api.octopus.energy/v1/products/SILVER-FLEX-22-11-25/electricity-tariffs/E-1R-SILVER-FLEX-22-11-25-${region}/standard-unit-rates/`);
@@ -13,11 +24,46 @@
     return elec;
   }
 
+  function createChart(element, data, color, min = 0) {
+    new Chart(
+      document.getElementById(element),
+      {
+        type: 'bar',
+        data: {
+          labels: data.map(row => new Date(row.valid_from).toLocaleDateString("en-gb")),
+          datasets: [
+            {
+              label: 'Prices Over Time',
+              data: data.map(row => row.value_inc_vat),
+              backgroundColor: color,
+            }
+          ]
+        },
+        options: {
+          scales: {
+            y: {
+              min: min
+            }
+          },
+          plugins: {
+            customCanvasBackgroundColor: {
+              color: 'white',
+            }
+          }
+        },
+        plugins: [plugin],
+      }
+    );
+  };
+
   getElec("B").then(elecValues => {
     var elec = document.getElementById("elec")
     var date = new Date(elecValues.results[0].valid_from).toDateString();
 
     elec.innerText = `${elecValues.results[0].value_inc_vat}p on ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`
+
+    var last10 = elecValues.results.slice(0, 10).reverse();
+    createChart("elecChart", last10, '#FFB1C1', 10)
   });
 
   getGas("B").then(gasValues => {
@@ -25,6 +71,8 @@
     var date = new Date(gasValues.results[0].valid_from).toDateString();
 
     gas.innerText = `${gasValues.results[0].value_inc_vat}p on ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`
+    var last10 = gasValues.results.slice(0, 10).reverse();
+    createChart("gasChart", last10, '#000000', 4)
   });
 
 
@@ -33,6 +81,8 @@
     var date = new Date(elecValues.results[0].valid_from).toDateString();
 
     elec.innerText = `${elecValues.results[0].value_inc_vat}p on ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`
+    var last10 = elecValues.results.slice(0, 10).reverse();
+    createChart("elecYChart", last10, '#FFB1C1', 10)
   });
 
   getGas("M").then(gasValues => {
@@ -40,6 +90,10 @@
     var date = new Date(gasValues.results[0].valid_from).toDateString();
 
     gas.innerText = `${gasValues.results[0].value_inc_vat}p on ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`
+    var last10 = gasValues.results.slice(0, 10).reverse();
+    createChart("gasYChart", last10, '#000000', 4)
   });
 })();
+
+
 
