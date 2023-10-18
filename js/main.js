71736595786fd1e35880465bd380064be683faf3
Jack Thorley <jack.thorley@myunidays.com>
2023-10-18 11:46:23 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -15,16 +15,16 @@
 
   getElec().then(elecValues => {
     var elec = document.getElementById("elec")
-    var date = new Date(elecValues.results[0].valid_to).toDateString();
+    var date = new Date(elecValues.results[0].valid_from).toDateString();
 
-    elec.innerText = `${elecValues.results[0].value_inc_vat}p on date ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`
+    elec.innerText = `${elecValues.results[0].value_inc_vat}p on ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`
   });
 
   getGas().then(gasValues => {
     var gas = document.getElementById("gas")
-    var date = new Date(gasValues.results[0].valid_to).toDateString();
+    var date = new Date(gasValues.results[0].valid_from).toDateString();
 
-    gas.innerText = `${gasValues.results[0].value_inc_vat}p on date ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`
+    gas.innerText = `${gasValues.results[0].value_inc_vat}p on ${date.toLocaleString('en-GB', { timeZone: 'UTC' })}`
   });
 })();
 
